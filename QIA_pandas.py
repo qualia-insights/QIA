15f811d183efa28596cde02c95743bef167d28b7
Todd Rovito <rovitotv@gmail.com>
2021-02-08 02:55:39 +0000

    new commit of QIA with Pandas

---
+++ QIA_pandas.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+
+def load_csv_data(path_to_data):
+    '''
+        provided the path to the data load the data but skip the
+        first line because it is garbage
+    '''
+    only_files = [f for f in listdir(path_to_data) if isfile(join(path_to_data, f))]
+    only_files.sort()
+    #bank_data = None
+    for file_index in range(0, len(only_files)):
+        csv_file_name = path_to_data + "/" + only_files[file_index]
+        print("processing %s" % csv_file_name)
+        month_data = pd.read_csv(csv_file_name, header=None, skiprows=1, names=['date', 'amount', 'description', 'not_known1', 'not_known2','type'],
+                       parse_dates=True)
+    '''
+        if len(month_data) > 0:
+            if bank_data == None:
+                bank_data = month_data
+            else:
+                bank_data = pd.concat(month_data, bank_data)
+        
+    # now we have read the data but the format is not correct, so below
+    # we reformat the types to the proper type
+    new_bank_data = []
+    for index in range(0, len(bank_data)):
+        date_str = bank_data[index][0]
+        year = int(date_str.split("/")[0])
+        month = int(date_str.split("/")[1])
+        day = int(date_str.split("/")[2])
+        new_row = {
+                    'date': date(year, month, day),
+                    'amount': float(bank_data[index][1]),
+                    'description_1': bank_data[index][2],
+                    'description_2': bank_data[index][3],
+                    'description_3': bank_data[index][4],
+                    'type': bank_data[index][5],
+        }
+        if new_row['type'] == 'DEBIT':
+            new_row['amount'] = new_row['amount'] * -1
+        new_bank_data.append(new_row)
+
+    return new_bank_data
+    '''
+
+if __name__ == "__main__":
+    print("Welcome to QI Pandas Accounting System verion 0.1 by Todd V. Rovito rovitotv@gmail.com")
+    # for raspberry pi rwind data is /home/rovitotv/data/QIA
+    load_csv_data("/home/rovitotv/data/QIA_data/2020/")
---
+++ README.md
@@ -0,0 +1,52 @@
+Qualia Insights Accounting System...why buy when you can write
+your own system? My lovely Wife owns and operates a mental health
+counselling business and I do her accounting.  For the last several
+years I have used this simple Python script.  We bank with PNC so
+it imports the CSV files and puts categories on everything.  The
+system is not perfect so feel free to modify for your needs.
+
+QIA is based on Python it runs on Raspberry Pi, Chromebook (using crouton with
+Anaconda), iPad (using Pythonista), and MacOS (using Anaconda).  It is meant
+to be run from the iPython prompt interactively. All the modules used are well
+known and easy to install. The list of Python dependencies are:
+* matplotlib
+* numpy
+* Python Standard Library modules: CSV, datetime, os
+
+# General Steps
+
+0. Export the month's bank account information from PNCBank.com
+
+1. set the variable path_to_data
+```python
+path_to_data = '/home/pi/qualia_insights_accounting' + "/data/2017"
+```
+
+2. run QIA.py in iPython...note nothing will run it will just load
+the functions in QIA.py.
+
+3. load the csv data
+```python
+bank_data = load_csv_data(path_to_data)
+```
+
+4. Read the categories csv file
+```python
+categories = read_categories("/home/pi/qualia_insights_accounting/data/categories.csv")
+```
+
+# To-Do List
+
+
+0. Generate monthly reports print out in html
+
+1. Generate yearly reports print out in html
+
+2. Save graphs as images then place into reports
+
+3. Make a different in transactions for personal and business.  For example
+we might have an expense that is paid with personal.  I am thinking have
+different names for the data files.
+
+4. Check out https://plaintextaccounting.org/. Specifically https://github.com/beancount/beancount/
+
